c2df03dc0de7231cf820473a9e81849f3b411d9a
user7888 <junnoelias@gmail.com>
2021-12-21 20:32:17 +0200

    step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+
+  const addNumber = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const numberObject = {
+      name: newName,
+      number : newNumber
+    }
+    // else-rakenne / totuusarvot?
+    if (isInArray(numberObject)) {
+      console.log('Nimi löytyi listasta..')
+      return
+    }
+    setPersons(persons.concat(numberObject))
+    setNewName('')
+  }
+  const handleNameChange = (event) => {
+    console.log('Event-olion kenttä', event.target.value)
+    setNewName(event.target.value)
+  }
+  // Kun event tapahtuu, muutetaan tilaa.
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+  // haun listalta tietyllä attribuutilla voi tehdä
+  // myös .some metodilla. (ilman boolean-muunnosta?)
+  const isInArray = (element) => {
+    console.log(element.name)
+    const nameFound = Boolean(persons.find(person => person.name === element.name))
+    if (nameFound) {
+      window.alert(`${element.name} is already added to phone book`)
+      return true
+    }
+    return false
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNumber}>
+        <div> name: <input 
+            value={newName}
+            onChange={handleNameChange}/>
+        </div>
+        <div>
+           number: <input
+            value={newNumber}
+            onChange={handleNumberChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+        {persons.map(person =>
+        <div key={person.name}>
+          <Numbers name={person.name} number={person.number}/>
+        </div>)
+        }
     </div>
-  );
+  )
 }
 
-export default App;
+const Numbers = (props) => {
+  return (
+    <p>{props.name} {props.number}</p>
+  )
+}
+
+export default App
