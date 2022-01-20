ba1e1d300d2480c72017dd8e29a2e32bd37c0fdf
user7888 <junnoelias@gmail.com>
2022-01-20 14:17:27 +0200

    project refactored

--- osa4/blog-list-backend/index.js
+++ osa4/blog-list-backend/index.js
@@ -1,54 +1,11 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
+const logger = require('./utils/logger')
+const config = require('./utils/config')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
-//const mongoUrl = 'mongodb://localhost/bloglist'
-//mongoose.connect(mongoUrl)
-console.log('connecting to', mongoUrl)
-mongoose
-  .connect(mongoUrl)
-  .then(() => {
-    console.log('connected to MongoDB')
-  })
-  .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
-  })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+const server = http.createServer(app)
 
 const PORT = 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
