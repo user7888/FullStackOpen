fb6f1c95757c4202035babd7d639b68546213291
user7888 <junnoelias@gmail.com>
2022-01-21 15:45:00 +0200

    indentation fix

--- osa4/blog-list-backend/tests/list.test.js
+++ osa4/blog-list-backend/tests/list.test.js
@@ -61,74 +61,74 @@ describe('total likes', () => {
   })
 
   test('of a bigger list is calculated right', () => {
-		expect(listHelper.totalLikes(testList)).toBe(36)
+    expect(listHelper.totalLikes(testList)).toBe(36)
   })
 })
 
 describe('the most liked blog', () => {
-	test('of an empty list cant be calculated and the test returns a string', () => {
-		expect(listHelper.favouriteBlog([])).toEqual('List of blogs was empty')
-	})
+  test('of an empty list cant be calculated and the test returns a string', () => {
+    expect(listHelper.favouriteBlog([])).toEqual('List of blogs was empty')
+  })
 
-	test('when the list has only one blog, the most liked blog is equal to that blog', () => {
-		testBlog = {
-			title: testList[0].title,
-			author: testList[0].author,
-			likes: testList[0].likes
-		}
-		expect(listHelper.favouriteBlog([testList[0]])).toEqual(testBlog)
-	})
+  test('when the list has only one blog, the most liked blog is equal to that blog', () => {
+    testBlog = {
+      title: testList[0].title,
+      author: testList[0].author,
+      likes: testList[0].likes
+    }
+    expect(listHelper.favouriteBlog([testList[0]])).toEqual(testBlog)
+  })
 
-	test('of a bigger list is calculated right', () => {
-		testBlog = {
-			title: testList[2].title,
-			author: testList[2].author,
-			likes: testList[2].likes
-		}
-		expect(listHelper.favouriteBlog(testList)).toEqual(testBlog)
-	})
+  test('of a bigger list is calculated right', () => {
+    testBlog = {
+      title: testList[2].title,
+      author: testList[2].author,
+      likes: testList[2].likes
+    }
+    expect(listHelper.favouriteBlog(testList)).toEqual(testBlog)
+  })
 })
 
 describe('Author with most blogs', () => {
-	test('when the list is empty', () => {
-		expect(listHelper.mostBlogs([])).toEqual('List of blogs was empty')
-	})
+  test('when the list is empty', () => {
+    expect(listHelper.mostBlogs([])).toEqual('List of blogs was empty')
+  })
 
-	test('when the list has only one blog should be equal to author of that blog', () => {
-		testBlog = {
-			author: testList[0].author,
-			blogs: 1
-		}
-		expect(listHelper.mostBlogs([testList[0]])).toEqual(testBlog)
-	})
+  test('when the list has only one blog should be equal to author of that blog', () => {
+    testBlog = {
+      author: testList[0].author,
+      blogs: 1
+    }
+    expect(listHelper.mostBlogs([testList[0]])).toEqual(testBlog)
+  })
 
-	test('from a bigger list is calculated correctly', () => {
-		testBlog = {
-			author: "Robert C. Martin",
-			blogs: 3
-		}
-		expect(listHelper.mostBlogs(testList)).toEqual(testBlog)
-	})
+  test('from a bigger list is calculated correctly', () => {
+    testBlog = {
+      author: "Robert C. Martin",
+      blogs: 3
+    }
+    expect(listHelper.mostBlogs(testList)).toEqual(testBlog)
+  })
 })
 
 describe('Author with most likes', () => {
-	test('when the list is empty', () => {
-		expect(listHelper.mostLikes([])).toEqual('List of blogs was empty')
-	})
+  test('when the list is empty', () => {
+    expect(listHelper.mostLikes([])).toEqual('List of blogs was empty')
+  })
 
-	test('when the list has only one blog should equal to author of that blog', () => {
-		testBlog = {
-			author: testList[0].author,
-			likes: 7
-		}
-		expect(listHelper.mostLikes([testBlog])).toEqual(testBlog)
-	})
+  test('when the list has only one blog should equal to author of that blog', () => {
+    testBlog = {
+      author: testList[0].author,
+      likes: 7
+    }
+    expect(listHelper.mostLikes([testBlog])).toEqual(testBlog)
+  })
 
-	test('from a bigger list is calculated correctly', () => {
-		testBlog = {
-			author: "Edsger W. Dijkstra",
-  		likes: 17
-		}
-		expect(listHelper.mostLikes(testList)).toEqual(testBlog)
-	})
+  test('from a bigger list is calculated correctly', () => {
+    testBlog = {
+      author: "Edsger W. Dijkstra",
+      likes: 17
+    }
+    expect(listHelper.mostLikes(testList)).toEqual(testBlog)
+  })
 })
